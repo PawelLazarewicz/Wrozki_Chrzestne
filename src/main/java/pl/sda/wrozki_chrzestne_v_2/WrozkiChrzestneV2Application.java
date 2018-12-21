89bcbabfcb9b90e979a974003c03e12d4e94ae79
Paweł <pl85@o2.pl>
2018-12-21 18:31:45 +0100

    initial commit

---
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/WrozkiChrzestneV2Application.java
@@ -0,0 +1,14 @@
+package pl.sda.wrozki_chrzestne_v_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrozkiChrzestneV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrozkiChrzestneV2Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/pl/sda/wrozki_chrzestne_v_2/WrozkiChrzestneV2ApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.sda.wrozki_chrzestne_v_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WrozkiChrzestneV2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
