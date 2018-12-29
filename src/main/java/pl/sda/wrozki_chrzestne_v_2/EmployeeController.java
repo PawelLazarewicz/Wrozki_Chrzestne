d56854d14aede25a558622a552e1a950e1f26feb
Paweł <pl85@o2.pl>
2018-12-29 07:47:35 +0100

    addEmployee()

--- src/main/java/pl/sda/wrozki_chrzestne_v_2/EmployeeController.java
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/EmployeeController.java
@@ -1,6 +1,10 @@
 package pl.sda.wrozki_chrzestne_v_2;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,4 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     private EmployeeRepository employeeRepository;
+
+    @PostMapping("addEmployee")
+    public ResponseEntity addEmployee(@RequestBody EmployeeDto employeeDto){
+        Employee newEmployee = new Employee(null,employeeDto.getName(), employeeDto.getLastName(), employeeDto.getCity(), employeeDto.getAge(), employeeDto.getTelephoneNumber(), employeeDto.getMail());
+        employeeRepository.save(newEmployee);
+
+        EmployeeDto newEmployeeDto = new EmployeeDto(newEmployee.getName(), newEmployee.getLastName(), newEmployee.getCity(), newEmployee.getAge(), newEmployee.getTelephoneNumber(), newEmployee.getMail());
+        return new ResponseEntity(newEmployeeDto, HttpStatus.OK);
+    }
 }
