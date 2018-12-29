b3df19db309e6a628838bf65be34a017641455fd
Paweł <pl85@o2.pl>
2018-12-29 07:26:28 +0100

    EmployeeController

--- src/main/java/pl/sda/wrozki_chrzestne_v_2/EmployeeRepository.java
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/EmployeeRepository.java
@@ -2,5 +2,5 @@ package pl.sda.wrozki_chrzestne_v_2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeService extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
