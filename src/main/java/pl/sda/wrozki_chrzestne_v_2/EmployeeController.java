56765c960444ad631f2e71629159b2f40c4d4f61
Paweł <pl85@o2.pl>
2018-12-30 09:14:24 +0100

    getEmployee()

--- src/main/java/pl/sda/wrozki_chrzestne_v_2/EmployeeController.java
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/EmployeeController.java
@@ -3,10 +3,7 @@ package pl.sda.wrozki_chrzestne_v_2;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,21 +15,30 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     @PostMapping("addEmployee")
-    public ResponseEntity addEmployee(@RequestBody EmployeeDto employeeDto){
-        Employee newEmployee = new Employee(null,employeeDto.getName(), employeeDto.getLastName(), employeeDto.getCity(), employeeDto.getAge(), employeeDto.getTelephoneNumber(), employeeDto.getMail());
+    public ResponseEntity addEmployee(@RequestBody EmployeeDto employeeDto) {
+        Employee newEmployee = new Employee(null, employeeDto.getName(), employeeDto.getLastName(), employeeDto.getCity(), employeeDto.getAge(), employeeDto.getTelephoneNumber(), employeeDto.getMail());
         employeeRepository.save(newEmployee);
 
         EmployeeDto newEmployeeDto = new EmployeeDto(newEmployee.getName(), newEmployee.getLastName(), newEmployee.getCity(), newEmployee.getAge(), newEmployee.getTelephoneNumber(), newEmployee.getMail());
         return new ResponseEntity(newEmployeeDto, HttpStatus.OK);
     }
 
-    @GetMapping ("listEmployees")
-    public ResponseEntity allEmployees(){
+    @GetMapping("listEmployees")
+    public ResponseEntity allEmployees() {
         List<Employee> employeeList = employeeRepository.findAll();
         List<EmployeeDto> employeeDtos = employeeList.stream()
                 .map(e -> new EmployeeDto(e.getName(), e.getLastName(), e.getCity(), e.getAge(), e.getTelephoneNumber(), e.getMail()))
                 .collect(Collectors.toList());
 
         return new ResponseEntity(employeeDtos, HttpStatus.OK);
+    }
+
+    @GetMapping("Employee/{id}")
+    public ResponseEntity getEmployee(@PathVariable Long id) {
+        Employee selectedEmployee = employeeRepository.getOne(id);
+
+        EmployeeDto selectedEmployeeDto = new EmployeeDto(selectedEmployee.getName(), selectedEmployee.getLastName(), selectedEmployee.getCity(), selectedEmployee.getAge(), selectedEmployee.getTelephoneNumber(), selectedEmployee.getMail());
+
+        return new ResponseEntity(selectedEmployeeDto, HttpStatus.OK);
     }
 }
