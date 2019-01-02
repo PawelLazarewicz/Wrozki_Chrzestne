0d4fd18bcb5e79d9774e215bf1bf5b230a7e7859
Paweł <pl85@o2.pl>
2019-01-02 18:29:51 +0100

    creating JobRepository interface; JobController class

---
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/JobController.java
@@ -0,0 +1,11 @@
+package pl.sda.wrozki_chrzestne_v_2;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class JobController {
+
+    private JobRepository jobRepository;
+}
---
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/JobRepository.java
@@ -0,0 +1,6 @@
+package pl.sda.wrozki_chrzestne_v_2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
