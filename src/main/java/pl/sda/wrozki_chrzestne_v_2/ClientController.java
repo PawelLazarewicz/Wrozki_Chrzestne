3e909f35cc52084dcc24d389647f511c78d9bc37
Paweł <pl85@o2.pl>
2019-01-03 15:25:06 +0100

    addClient()

--- src/main/java/pl/sda/wrozki_chrzestne_v_2/ClientController.java
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/ClientController.java
@@ -1,6 +1,10 @@
 package pl.sda.wrozki_chrzestne_v_2;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,4 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClientController {
 
     private ClientRepository clientRepository;
+    
+    private ClientBuilderService clientBuilderService;
+
+    @PostMapping("addClient")
+    public ResponseEntity addClient(@RequestBody ClientDto clientDto) {
+        Client newClient = clientBuilderService.entityFromDto(clientDto);
+        clientRepository.save(newClient);
+
+        ClientDto newClientDto = clientBuilderService.DtoFromEntity(newClient);
+        return new ResponseEntity(newClientDto, HttpStatus.OK);
+    }
 }
