2d0806bb5f9c2a14ae460ed26018e7eba3284e54
Paweł <pl85@o2.pl>
2019-01-03 15:27:28 +0100

    allClients()

--- src/main/java/pl/sda/wrozki_chrzestne_v_2/ClientController.java
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/ClientController.java
@@ -3,16 +3,20 @@ package pl.sda.wrozki_chrzestne_v_2;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
 public class ClientController {
 
     private ClientRepository clientRepository;
-    
+
     private ClientBuilderService clientBuilderService;
 
     @PostMapping("addClient")
@@ -22,5 +26,15 @@ public class ClientController {
 
         ClientDto newClientDto = clientBuilderService.DtoFromEntity(newClient);
         return new ResponseEntity(newClientDto, HttpStatus.OK);
+    }
+
+    @GetMapping("listClients")
+    public ResponseEntity allClients() {
+        List<Client> clientList = clientRepository.findAll();
+        List<ClientDto> clientDtos = clientList.stream()
+                .map(e -> clientBuilderService.DtoFromEntity(e))
+                .collect(Collectors.toList());
+
+        return new ResponseEntity(clientDtos, HttpStatus.OK);
     }
 }
