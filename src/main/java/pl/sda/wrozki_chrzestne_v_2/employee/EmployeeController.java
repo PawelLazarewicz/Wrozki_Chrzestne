ffd9a29b33ce8c4ea9390a2903eb7357198f9994
Paweł <pl85@o2.pl>
2019-01-06 16:31:15 +0100

    getEmployee()

--- src/main/java/pl/sda/wrozki_chrzestne_v_2/employee/EmployeeController.java
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/employee/EmployeeController.java
@@ -45,4 +45,48 @@ public class EmployeeController {
 
         return "employeesHTML";
     }
+
+    @RequestMapping("Employee/show/{id}")
+    public String getEmployee(@PathVariable Long id, Model model) {
+        Employee selectedEmployee = employeeRepository.getOne(id);
+
+        EmployeeDto selectedEmployeeDto = employeeBuilderService.DtoFromEntity(selectedEmployee);
+
+        model.addAttribute("employee", selectedEmployeeDto);
+
+        return "employeeHTML";
+    }
+
+//    @GetMapping("Employee/{id}/delete")
+//    public ResponseEntity deleteEmployee(@PathVariable Long id) {
+//        Employee selectedEmployee = employeeRepository.getOne(id);
+//
+//        EmployeeDto selectedEmployeeDto = employeeBuilderService.DtoFromEntity(selectedEmployee);
+//
+//        employeeRepository.delete(selectedEmployee);
+//
+//        return new ResponseEntity(selectedEmployeeDto, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("Employee/{id}/move")
+//    public ResponseEntity moveEmployee(@PathVariable Long id) {
+//        Employee selectedEmployee = employeeRepository.getOne(id);
+//
+//        EmployeeDto selectedEmployeeDto = employeeBuilderService.DtoFromEntity(selectedEmployee);
+//
+//        inactiveEmployees.add(selectedEmployee);
+//        employeeRepository.delete(selectedEmployee);
+//
+//        return new ResponseEntity(selectedEmployeeDto, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("listEmployees/inactive")
+//    public ResponseEntity allInactiveEmployees() {
+//        List<Employee> inactiveEmployeeList = new ArrayList<>(inactiveEmployees);
+//        List<EmployeeDto> inactiveEmployeeDtos = inactiveEmployeeList.stream()
+//                .map(e -> employeeBuilderService.DtoFromEntity(e))
+//                .collect(Collectors.toList());
+//
+//        return new ResponseEntity(inactiveEmployeeDtos, HttpStatus.OK);
+//    }
 }
