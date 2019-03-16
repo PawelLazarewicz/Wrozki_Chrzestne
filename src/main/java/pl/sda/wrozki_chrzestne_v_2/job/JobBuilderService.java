9a813df9ea12d2e6259c77e50f517fe586dabd59
Paweł <pl85@o2.pl>
2019-03-16 20:43:11 +0100

    JobBuilderService.java - deleting comments

--- src/main/java/pl/sda/wrozki_chrzestne_v_2/job/JobBuilderService.java
+++ src/main/java/pl/sda/wrozki_chrzestne_v_2/job/JobBuilderService.java
@@ -26,27 +26,17 @@ public class JobBuilderService {
 
     public Job entityFromDto(JobDto jobDto) {
         Job job = new Job();
-
         job.setId(null);
 
         Client client = clientBuilderService.entityFromDto(jobDto.getClient());
         job.setClient(client);
 
         jobCoreEntityFromDto(job, jobDto);
-//        job.setDateOfJob(jobDto.getDateOfJob());
-//        job.setCity(jobDto.getCity());
-//        job.setJobsAddress(jobDto.getJobsAddress());
-//        job.setJobsPostalCode(jobDto.getJobsPostalCode());
-//        job.setSortOfJob(jobDto.getSortOfJob());
-//        job.setEstimatedTime(jobDto.getEstimatedTime());
-//        job.setNumberOfChildren(jobDto.getNumberOfChildren());
-//        job.setJobStatus(jobDto.getJobStatus());
 
         List<Employee> employees = jobDto.getEmployees()
                 .stream()
                 .map(e -> employeeBuilderService.entityFromDto(e))
                 .collect(Collectors.toList());
-
         job.setEmployees(employees);
 
         return job;
@@ -54,7 +44,6 @@ public class JobBuilderService {
 
     public JobDto dtoFromEntity(Job job) {
         JobDto jobDto = new JobDto();
-
         jobDto.setId(job.getId());
 
         ClientDto clientDto = clientBuilderService.dtoFromEntity(job.getClient());
@@ -74,25 +63,11 @@ public class JobBuilderService {
 
     public JobDto dtoFromEntityWithEmployees(Job job) {
         JobDto jobDto = dtoFromEntity(job);
-//        jobDto.setId(job.getId());
-//
-//        ClientDto clientDto = clientBuilderService.dtoFromEntity(job.getClient());
-//        jobDto.setClient(clientDto);
-//
-//        jobDto.setDateOfJob(job.getDateOfJob());
-//        jobDto.setCity(job.getCity());
-//        jobDto.setJobsAddress(job.getJobsAddress());
-//        jobDto.setJobsPostalCode(job.getJobsPostalCode());
-//        jobDto.setSortOfJob(job.getSortOfJob());
-//        jobDto.setEstimatedTime(job.getEstimatedTime());
-//        jobDto.setNumberOfChildren(job.getNumberOfChildren());
-//        jobDto.setJobStatus(job.getJobStatus());
 
         List<EmployeeDto> employeesDto = job.getEmployees()
                 .stream()
                 .map(e -> employeeBuilderService.dtoFromEntity(e))
                 .collect(Collectors.toList());
-
         jobDto.setEmployees(employeesDto);
 
         return jobDto;
@@ -106,21 +81,12 @@ public class JobBuilderService {
     }
 
     public Job updateEntityFromDto(JobDto jobDto, Job job) {
-
         job.setId(job.getId());
 
         Client client = clientBuilderService.selectClientFromDto(jobDto.getClient());
         job.setClient(client);
 
         jobCoreEntityFromDto(job, jobDto);
-//        job.setDateOfJob(jobDto.getDateOfJob());
-//        job.setCity(jobDto.getCity());
-//        job.setJobsAddress(jobDto.getJobsAddress());
-//        job.setJobsPostalCode(jobDto.getJobsPostalCode());
-//        job.setSortOfJob(jobDto.getSortOfJob());
-//        job.setEstimatedTime(jobDto.getEstimatedTime());
-//        job.setNumberOfChildren(jobDto.getNumberOfChildren());
-//        job.setJobStatus(jobDto.getJobStatus());
 
         List<Employee> employees = jobDto.getEmployees()
                 .stream()
@@ -128,64 +94,24 @@ public class JobBuilderService {
                         .stream()
                         .filter(employee -> employee.getId().equals(e.getId())).findFirst().get()))
                 .collect(Collectors.toList());
-
         job.setEmployees(employees);
 
         return job;
     }
 
-//    public Job updateEntityFromDtoWithClient(JobDto jobDto, Job job) {
-//
-//        job.setId(job.getId());
-//
-//        Client client = clientBuilderService.selectClientFromDto(jobDto.getClient());
-//        job.setClient(client);
-//
-//        jobCoreEntityFromDto(job, jobDto);
-//
-//        List<Employee> employees = jobDto.getEmployees()
-//                .stream()
-//                .map(e -> employeeBuilderService.updateEntityFromDto(e, job.getEmployees()
-//                        .stream()
-//                        .filter(employee -> employee.getId().equals(e.getId())).findFirst().get()))
-//                .collect(Collectors.toList());
-//
-//        job.setEmployees(employees);
-////        job.setDateOfJob(jobDto.getDateOfJob());
-////        job.setCity(jobDto.getCity());
-////        job.setJobsAddress(jobDto.getJobsAddress());
-////        job.setJobsPostalCode(jobDto.getJobsPostalCode());
-////        job.setSortOfJob(jobDto.getSortOfJob());
-////        job.setEstimatedTime(jobDto.getEstimatedTime());
-////        job.setNumberOfChildren(jobDto.getNumberOfChildren());
-////        job.setJobStatus(jobDto.getJobStatus());
-//
-//        return job;
-//    }
-
     public Job entityFromDtoWithUpdatingClient(JobDto jobDto, ClientDto clientDto) {
         Job job = new Job();
-
         job.setId(null);
 
         Client client = clientBuilderService.selectClientFromDto(clientDto);
         job.setClient(client);
 
         jobCoreEntityFromDto(job, jobDto);
-//        job.setDateOfJob(jobDto.getDateOfJob());
-//        job.setCity(jobDto.getCity());
-//        job.setJobsAddress(jobDto.getJobsAddress());
-//        job.setJobsPostalCode(jobDto.getJobsPostalCode());
-//        job.setSortOfJob(jobDto.getSortOfJob());
-//        job.setEstimatedTime(jobDto.getEstimatedTime());
-//        job.setNumberOfChildren(jobDto.getNumberOfChildren());
-//        job.setJobStatus(jobDto.getJobStatus());
 
         List<Employee> employees = jobDto.getEmployees()
                 .stream()
                 .map(e -> employeeBuilderService.entityFromDto(e))
                 .collect(Collectors.toList());
-
         job.setEmployees(employees);
 
         return job;
